cd86e26bf46c73993a5107774fd3636071ed4ac8
lindaiiako <lmsarmiento@unist.ac.kr>
2025-09-05 15:31:21 +0900

    replace pitch tracker

--- AutomaticPipeline/PitchEnergy.py
+++ AutomaticPipeline/PitchEnergy.py
@@ -2,15 +2,16 @@ import librosa
 import numpy as np
 
 def extract_pitch(wav, sr):
-    pitches, magnitudes = librosa.core.piptrack(y=wav, sr=sr)
-    return pitches
-
-def calculate_mean_pitch(pitches):
-    return np.mean([np.mean(p[p > 0]) for p in pitches.T if np.sum(p > 0) > 0])
+    f0, voiced_flag, _= librosa.pyin(
+                wav, 
+                fmin=65, 
+                fmax=400,
+                sr=sr)
+    return f0[voiced_flag]
 
 def process_audio(audio, sr):
-    pitches = extract_pitch(audio, sr = sr)
-    mean_pitch = calculate_mean_pitch(pitches)
+    f0_values = extract_pitch(audio, sr = sr)
+    mean_pitch = f0_values.mean()
     energy = librosa.feature.rms(y=audio)
     mean_energy = np.mean(energy[~np.isnan(energy)])
 
